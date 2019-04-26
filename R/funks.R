0aca96f20f95ddb1d924abb218a07068f1fec655
Dr Asis Hallab <a.hallab@fz-juelich.de>
2019-04-26 22:54:45 +0200

    Got generation of MEME HyPhy batch-files working.

--- R/funks.R
+++ R/funks.R
@@ -45,6 +45,71 @@ createGeneFamilyWorkingDirIfNotExists <- function(base.dir, gene.family.name) {
     gene.fam.wd
 }
 
+#' Creates the argument gene family 'gene.family.name' mapping of original gene
+#' identifiers to sanitized 'PROT1' ... 'PROTN' identifiers.
+#'
+#' @param gene.family.dir - The valid file path to the gene family's working
+#' directory.
+#' @param gene.family.name - The name of the gene family which defines the name
+#' of the directory to be created within the argument 'base.dir'.
+#' @param gene.fams.lst - The list of gene families in which to lookup the
+#' argument 'gene.family.name'. Default is SlydGeneFamsAnalyses's
+#' \code{gene.families}.
+#'
+#' @return A data.frame holding two columns: original and sanitized. The
+#' dataframe is either loaded from a file 'name_mappings.txt' or created,
+#' stored in that file, and then returned.
+#' @export
+geneNameMappings <- function(gene.family.dir, gene.family.name, gene.fams.lst = gene.families) {
+    gene.fam.name.maps.path <- file.path(gene.family.dir, paste0(gene.family.name, 
+        "_name_mappings.txt"))
+    if (file.exists(gene.fam.name.maps.path)) {
+        read.table(gene.fam.name.maps.path, header = TRUE, sep = "\t", 
+            stringsAsFactors = FALSE)
+    } else {
+        gf.name.maps.df <- data.frame(original = gene.fams.lst[[gene.family.name]], 
+            stringsAsFactors = FALSE)
+        gf.name.maps.df$sanitized <- paste0("PROT", 1:length(gene.fams.lst[[gene.family.name]]))
+        write.table(gf.name.maps.df, gene.fam.name.maps.path, row.names = FALSE, 
+            quote = FALSE, sep = "\t")
+        gf.name.maps.df
+    }
+}
+
+#' Returns the sanitized gene names mapped to the argument 'originals' as
+#' assigned in argument 'mappings' table.
+#'
+#' @param originals - Character vector of gene identifiers.
+#' @param mappings - Instance of base::data.frame with two columns: 'original'
+#' holding the original gene identifiers, and 'sanitized' holding the mapped
+#' simplified gene identifiers. See function \code{geneNameMappings} for more
+#' details.
+#'
+#' @return A character vector of same length as argument 'originals' holding
+#' the respective sanitized gene identifiers.
+#' @export
+mapOriginalToSanitizedNames <- function(originals, mappings) {
+    rownames(mappings) <- mappings$original
+    unlist(lapply(originals, function(x) mappings[[x, "sanitized"]]))
+}
+
+#' Returns the original gene names mapped to the argument 'sanitized' as
+#' assigned in argument 'mappings' table.
+#'
+#' @param sanitized - Character vector of gene identifiers.
+#' @param mappings - Instance of base::data.frame with two columns: 'original'
+#' holding the original gene identifiers, and 'sanitized' holding the mapped
+#' simplified gene identifiers. See function \code{geneNameMappings} for more
+#' details.
+#'
+#' @return A character vector of same length as argument 'sanitized' holding
+#' the respective original gene identifiers.
+#' @export
+mapSanitizedToOriginalNames <- function(sanitized, mappings) {
+    rownames(mappings) <- mappings$sanitized
+    unlist(lapply(sanitized, function(x) mappings[[x, "original"]]))
+}
+
 #' Loads all multiple amino acid sequence alignments (MSAs) for the respective
 #' gene families reconstructed by Orthofinder. They are read in using
 #' \code{seqinr::read.fasta} and gene identifiers are sanitized, removing the
@@ -82,6 +147,10 @@ loadAndSanitizeAAMsas <- function(path.2.msa.dir, working.dir, gene.families.nam
         }
         gene.fam.wd <- createGeneFamilyWorkingDirIfNotExists(working.dir, 
             gene.fam)
+        write.fasta(msa, names(msa), file.path(gene.fam.wd, paste0(gene.fam, 
+            "_AA_MSA_orig_gene_ids.fa")))
+        fam.gene.id.maps <- geneNameMappings(gene.fam.wd, gene.fam)
+        names(msa) <- mapOriginalToSanitizedNames(names(msa), fam.gene.id.maps)
         write.fasta(msa, names(msa), file.path(gene.fam.wd, paste0(gene.fam, 
             "_AA_MSA.fa")))
         NULL
@@ -127,12 +196,29 @@ loadAndSanitizeTrees <- function(path.2.trees.dir, working.dir, gene.families.na
         gene.fam.wd <- createGeneFamilyWorkingDirIfNotExists(working.dir, 
             gene.fam)
         write.tree(fam.tree, file.path(gene.fam.wd, paste0(gene.fam, 
+            "_phyl_tree_orig_gene_ids.newick")))
+        fam.gene.id.maps <- geneNameMappings(gene.fam.wd, gene.fam)
+        fam.tree$tip.label <- mapOriginalToSanitizedNames(fam.tree$tip.label, fam.gene.id.maps)
+        write.tree(fam.tree, file.path(gene.fam.wd, paste0(gene.fam, 
             "_phyl_tree.newick")))
         NULL
     })
     TRUE
 }
 
+#' Generates the coding sequence alignments for all families found in
+#' sub-directories of argument 'working.dir'.
+#'
+#' @param working.dir - The valid file path to the directory in which to store
+#' the sanitized and parsed MSAs. Each will be stored in its respective gene
+#' family's sub-directory, identified by the family's name itself.
+#' @param gene.families.names - A character vector of gene family names that
+#' are to be expected to contain the sub-directory names in argument
+#' 'working.dir' excluding the file-extensions. Default is this package's
+#' \code{names(gene.families)}.
+#'
+#' @return TRUE if and only if no error has occurred.
+#' @export
 generateCodingSequenceMSAs <- function(working.dir, gene.families.names = names(gene.families)) {
     gene.fams.dirs <- system(paste("ls", working.dir, "| grep -P \"^OG\""), 
         intern = TRUE)
@@ -144,11 +230,55 @@ generateCodingSequenceMSAs <- function(working.dir, gene.families.names = names(
         gene.fam.wd <- normalizePath(file.path(working.dir, gene.fam))
         fam.cds <- all.cds[gene.families[[gene.fam]]]
         fam.aa.msa <- seqinr::read.fasta(file.path(gene.fam.wd, paste0(gene.fam, 
-            "_AA_MSA.fa")), seqtype = "AA", as.string = TRUE, strip.desc = TRUE)
+            "_AA_MSA_orig_gene_ids.fa")), seqtype = "AA", as.string = TRUE, strip.desc = TRUE)
         fam.cds.msa <- GeneFamilies::alignCDSSetWithAlignedAAsAsGuide(fam.cds, 
             fam.aa.msa)
+        fam.gene.id.maps <- geneNameMappings(gene.fam.wd, gene.fam)
+        names(fam.cds.msa) <- mapOriginalToSanitizedNames(names(fam.cds.msa), fam.gene.id.maps)
         seqinr::write.fasta(fam.cds.msa, names(fam.cds.msa), file.path(gene.fam.wd, 
             paste0(gene.fam, "_CDS_MSA.fa")))
+        NULL
+    })
+    TRUE
+}
+
+#' Generates the MEME HyPhy batch-files for all families found in
+#' sub-directories of argument 'working.dir'.
+#'
+#' @param working.dir - The valid file path to the directory in which to store
+#' the sanitized and parsed MSAs. Each will be stored in its respective gene
+#' family's sub-directory, identified by the family's name itself.
+#' @param hyphy.batch.files.dir - The valid file path to the directory of the
+#' HyPhy installation in which to find the HyPhy batch files.
+#' @param gene.families.names - A character vector of gene family names that
+#' are to be expected to contain the sub-directory names in argument
+#' 'working.dir' excluding the file-extensions. Default is this package's
+#' \code{names(gene.families)}.
+#'
+#' @return TRUE if and only if no error has occurred.
+#' @export
+generateMemeBatchFiles <- function(working.dir, hyphy.batch.files.dir, 
+    gene.families.names = names(gene.families)) {
+    hyphy.batch.files.dir <- normalizePath(hyphy.batch.files.dir)
+    gene.fams.dirs <- system(paste("ls", working.dir, "| grep -P \"^OG\""), 
+        intern = TRUE)
+    if (!all(gene.fams.dirs %in% gene.families.names)) {
+        warning("Some of the gene families stored in working directory '", 
+            working.dir, "' do not appear in the argument gene.families.names!")
+    }
+    mclapply(gene.fams.dirs, function(gene.fam) {
+        gene.fam.wd <- normalizePath(file.path(working.dir, gene.fam))
+        fam.cds.msa.path <- file.path(gene.fam.wd, paste0(gene.fam, 
+            "_CDS_MSA.fa"))
+        fam.tree.no.node.labels.path <- file.path(gene.fam.wd, paste0(gene.fam, 
+            "_phyl_tree.newick"))
+        fam.hyphy.meme.log.path <- file.path(gene.fam.wd, paste0(gene.fam, 
+            "_HyPhy_MEME_log.txt"))
+        fam.hyphy.meme.output.path <- file.path(gene.fam.wd, paste0(gene.fam, 
+            "_HyPhy_MEME_output.txt"))
+        fam.hyphy.meme.batch.file.path <- file.path(gene.fam.wd, paste0(gene.fam, 
+            "_HyPhy_MEME_input.bf"))
+        brew(text = GeneFamilies::hyphy.meme.bf, output = fam.hyphy.meme.batch.file.path)
         NULL
     })
     TRUE
---
+++ exec/generateMemeHyPhyBatchFiles.R
@@ -0,0 +1,41 @@
+require(SlydGeneFamsAnalyses)
+
+option_list = list(
+  make_option(
+    c("-w", "--workDir"),
+    type = "character",
+    default = NULL,
+    help = "The directory into which to write each family's parsed phylogenetic tree and multiple amino acid sequence alignment.",
+    metavar = "character"
+  ),
+  make_option(
+    c("-h", "--HyPhyBatchFilesDir"),
+    type = "character",
+    default = NULL,
+    help = "The sub-directory of the HyPhy installation in which to find HyPhy's Batch-Files.",
+    metavar = "character"
+  ),
+  make_option(
+    c("-c", "--cores"),
+    type = "integer",
+    default = detectCores(),
+    help = "The number of cores to use in parallel. Default is the result of 'detectCores()', i.e. ALL.",
+    metavar = "integer"
+  )
+)
+
+opt_parser = OptionParser(option_list = option_list)
+args = parse_args(opt_parser)
+
+if (is.null(args$workDir) || is.null(args$HyPhyBatchFilesDir) {
+    stop("Please specify the required argument '--workDir' and '--HyPhyBatchFilesDir'.")
+}
+
+options(mc.cores = args$cores)
+message("Set mc.cores to ", args$cores)
+
+# Generate each family's multiple coding sequence alignment:
+generateMemeBatchFiles(args$workDir, args$HyPhyBatchFilesDir)
+
+# DONE
+message("DONE")
